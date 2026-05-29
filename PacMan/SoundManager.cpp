a0d28d0f02068b6079f7db49057dd9ea889ffc9b
Chloe-J1 <chloe.jonckheere@student.howest.be>
2026-05-29 13:35:25 +0200

    added sound effects

--- PacMan/SoundManager.cpp
+++ PacMan/SoundManager.cpp
@@ -9,7 +9,10 @@ pacman::SoundManager::SoundManager()
 	dae::SoundSystem* ss = dae::ServiceLocator::GetSoundSystem();
 	ss->RegisterSound("damage", "Data/Sound/death_1.wav");
 	ss->RegisterSound("button", "Data/Sound/button.mp3");
-	ss->RegisterSound("pickup_pellet", "Data/Sound/eat_dot.wav");
+	ss->RegisterSound("pellet_pickup", "Data/Sound/eat_dot.wav");
+	ss->RegisterSound("fruit_pickup", "Data/Sound/eat_fruit.wav");
+	ss->RegisterSound("player_died", "Data/Sound/ms_death.wav");
+	ss->RegisterSound("ghost_died", "Data/Sound/ms_eat_ghost.wav");
 }
 
 void pacman::SoundManager::Notify(dae::GameObject*, const dae::Event& event)
@@ -19,7 +22,6 @@ void pacman::SoundManager::Notify(dae::GameObject*, const dae::Event& event)
 	if (event.id == "PLAYER_TAKES_DAMAGE")
 	{
 		ss->Play("damage", volume);
-		ss->Play("damage", volume);
 	}
 	else if (event.id == "BUTTON_PRESSED")
 	{
@@ -27,6 +29,18 @@ void pacman::SoundManager::Notify(dae::GameObject*, const dae::Event& event)
 	}
 	else if (event.id == "PELLET_PICKUP" || event.id == "POWER_PELLET_PICKUP")
 	{
-		ss->Play("pickup_pellet", volume);
+		ss->Play("pellet_pickup", volume);
+	}
+	else if (event.id == "FRUIT_PICKUP")
+	{
+		ss->Play("fruit_pickup", volume);
+	}
+	else if (event.id == "PLAYER_DIED")
+	{
+		ss->Play("player_died", volume);
+	}
+	else if (event.id == "GHOST_DIED")
+	{
+		ss->Play("ghost_died", volume);
 	}
 }
