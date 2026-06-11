72841b7078da6d740d84b6fc4db356300af2e6ca
Chloe-J1 <chloe.jonckheere@student.howest.be>
2026-06-11 19:57:05 +0200

    power pellets in third level added

--- PacMan/SceneLoader.cpp
+++ PacMan/SceneLoader.cpp
@@ -38,6 +38,7 @@ void pacman::SceneLoader::GameScene(const std::string& levelname)
 	dae::Scene& scene = dae::SceneManager::GetInstance().CreateScene();
 
 	m_levelLoader->InitLevel(scene, levelname);
+	m_levelLoader->JsonToBin("Data/Levels.json", levelname);
 	
 	// FPS
 	std::unique_ptr<dae::GameObject> fpsgo = std::make_unique<dae::GameObject>();
