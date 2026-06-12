76a397f39ef9a14ad0928b17c76035bde09192ad
Chloe-J1 <chloe.jonckheere@student.howest.be>
2026-06-12 11:36:51 +0200

    No more separate Sound helper class

--- Minigin/SoundSystem.cpp
+++ Minigin/SoundSystem.cpp
@@ -25,84 +25,6 @@ void dae::NullSoundSystem::RegisterSound(const std::string&, const std::string&)
 // SDLSoundSystem
 //***************
 
-class Sound {
-public:
-	Sound(const std::string& path, MIX_Mixer* mixer)
-		: m_mixer(mixer), m_filepath(path)
-	{
-	}
-
-	~Sound()
-	{
-		for (const auto& track : m_tracks)
-		{
-			MIX_StopTrack(track, 0);
-			MIX_DestroyTrack(track);
-		}
-		MIX_DestroyAudio(m_audio);
-	}
-
-	bool IsLoaded() const { return m_audio != nullptr; }
-
-	void Load()
-	{
-		m_audio = MIX_LoadAudio(m_mixer, m_filepath.c_str(), false);
-		m_tracks.push_back(MIX_CreateTrack(m_mixer));
-	}
-
-	void SetVolume(float volume)
-	{
-		volume = std::clamp(volume, 0.f, 1.f);
-		for (auto& track : m_tracks)
-			MIX_SetTrackGain(track, volume);
-	}
-
-	void Play(bool isLooping)
-	{
-		for (auto& track : m_tracks)
-		{
-			if (not MIX_TrackPlaying(track))
-			{
-				MIX_SetTrackAudio(track, m_audio);
-				SDL_PropertiesID props{ SDL_CreateProperties() };
-				if (isLooping)
-				{
-					SDL_SetNumberProperty(props, MIX_PROP_PLAY_LOOPS_NUMBER, 50);
-				}
-				MIX_PlayTrack(track, props);
-				SDL_DestroyProperties(props);
-				return;
-			}
-		}
-
-		// No free tracks found so create a new one
-		MIX_Track* newTrack = MIX_CreateTrack(m_mixer);
-		MIX_SetTrackAudio(newTrack, m_audio);
-		SDL_PropertiesID props{ SDL_CreateProperties() };
-		if (isLooping)
-		{
-			SDL_SetNumberProperty(props, MIX_PROP_PLAY_LOOPS_NUMBER, 50);
-		}
-		MIX_PlayTrack(newTrack, props);
-		m_tracks.push_back(newTrack);
-	}
-
-	void Stop()
-	{
-		for (const auto& track : m_tracks)
-		{
-			if (MIX_TrackPlaying(track))
-				MIX_StopTrack(track, 0);
-		}
-	}
-
-private:
-	MIX_Mixer* m_mixer;
-	MIX_Audio* m_audio{};
-	std::vector<MIX_Track*> m_tracks{};
-	std::string m_filepath;
-};
-
 // SDL
 #ifdef __EMSCRIPTEN__
 class dae::SDLSoundSystem::SoundSystemImpl final
@@ -116,7 +38,7 @@ public:
 	{
 	}
 
-	void Stop(const std::string& soundId)
+	void Stop(const std::string&)
 	{
 	}
 
@@ -175,7 +97,7 @@ public:
 	void RegisterSound(const std::string& id, const std::string& path)
 	{
 		std::lock_guard<std::mutex> lock(m_mutex);
-		m_soundMap[id] = std::make_unique<Sound>(path, m_mixer);
+		m_soundMap[id] = std::make_shared<Sound>(path);
 	}
 
 	void ProcessRequests(std::stop_token stopToken)
@@ -190,31 +112,31 @@ public:
 			while (not m_pendingRequests.empty())
 			{
 				SoundMessage message = m_pendingRequests.front();
+				m_pendingRequests.pop();
 				if (not m_soundMap.contains(message.id)) continue;
 
 				// Play sound
 				std::shared_ptr<Sound> sound = m_soundMap[message.id];
 
-				m_pendingRequests.pop();
 
 				lock.unlock();
 				if (message.type == "Play")
 				{
-					if (not sound->IsLoaded())
-						sound->Load();
-					sound->SetVolume(message.volume);
-					sound->Play(false);
+					if (not IsLoaded(*sound))
+						Load(*sound);
+					SetVolume(*sound, message.volume);
+					PlaySound(*sound, false);
 				}
 				else if (message.type == "Stop")
 				{
-					sound->Stop();
+					StopSound(*sound);
 				}
 				else if (message.type == "Loop")
 				{
-					if (not sound->IsLoaded())
-						sound->Load();
-					sound->SetVolume(message.volume);
-					sound->Play(true);
+					if (not IsLoaded(*sound))
+						Load(*sound);
+					SetVolume(*sound, message.volume);
+					PlaySound(*sound, true);
 				}
 				
 				lock.lock();
@@ -222,12 +144,80 @@ public:
 		}
 	}
 private:
+	struct Sound
+	{
+		explicit Sound(const std::string& _filepath):
+			filepath{_filepath}
+		{ }
+
+		MIX_Audio* audio{};
+		std::vector<MIX_Track*> tracks{};
+		std::string filepath;
+	};
+
 	MIX_Mixer* m_mixer{};
 	std::jthread m_thread;
 	std::mutex m_mutex;
 	std::condition_variable m_conditionVar;
 	std::queue<SoundMessage> m_pendingRequests;
 	std::unordered_map<std::string, std::shared_ptr<Sound>> m_soundMap;
+
+	void PlaySound(Sound& sound, bool isLooping)
+	{
+		for (auto& track : sound.tracks)
+		{
+			if (not MIX_TrackPlaying(track))
+			{
+				MIX_SetTrackAudio(track, sound.audio);
+				SDL_PropertiesID props{ SDL_CreateProperties() };
+				if (isLooping)
+				{
+					SDL_SetNumberProperty(props, MIX_PROP_PLAY_LOOPS_NUMBER, 50);
+				}
+				MIX_PlayTrack(track, props);
+				SDL_DestroyProperties(props);
+				return;
+			}
+		}
+			
+		// No free tracks found so create a new one
+		MIX_Track* newTrack = MIX_CreateTrack(m_mixer);
+		MIX_SetTrackAudio(newTrack, sound.audio);
+		SDL_PropertiesID props{ SDL_CreateProperties() };
+		if (isLooping)
+		{
+			SDL_SetNumberProperty(props, MIX_PROP_PLAY_LOOPS_NUMBER, 50);
+		}
+		MIX_PlayTrack(newTrack, props);
+		sound.tracks.push_back(newTrack);
+	}
+
+	void StopSound(const Sound& sound)
+	{
+		for (const auto& track : sound.tracks)
+		{
+			if (MIX_TrackPlaying(track))
+				MIX_StopTrack(track, 0);
+		}
+	}
+
+	bool IsLoaded(const Sound& sound)
+	{
+		return sound.audio != nullptr;
+	}
+
+	void Load(Sound& sound)
+	{
+		sound.audio = MIX_LoadAudio(m_mixer, sound.filepath.c_str(), false);
+		sound.tracks.push_back(MIX_CreateTrack(m_mixer));
+	}
+
+	void SetVolume(Sound& sound, float volume)
+	{
+		volume = std::clamp(volume, 0.f, 1.f);
+		for (auto& track : sound.tracks)
+			MIX_SetTrackGain(track, volume);
+	}
 };
 #endif
 
